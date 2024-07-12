ea19e415785c7f0e708e42e657605469ba71d433
Smart-imran <rst.imran4@gmail.com>
2024-07-13 00:45:13 +0600

    new page

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 //step-1: add click evvent handler with the submit button
-document.getElementById("btn-submit").addEventListener("click",function(){
+document.getElementById("btn-submit").addEventListener("click", function () {
     //step-2: get the email addres in side the Email input field 
     //always remember to use .value to get text from an input field
     const emailField = document.getElementById("user-email");
@@ -8,20 +8,21 @@ document.getElementById("btn-submit").addEventListener("click",function(){
     //step-3.a: set id on the html element
     //step-3.b:get the element 
     //step-3.c:get the value from the element
-    const passwordField =document.getElementById("user-password");
-    const password =passwordField.value;
-     
+    const passwordField = document.getElementById("user-password");
+    const password = passwordField.value;
+
 
     //Don't verify email password on the client side
-    
+
     //step-4: verify email and password
 
-    if(email==="imran@gmail.com" && password ==="imran"){
-        console.log("Valid user");
+    if (email === "imran@gmail.com" && password === "imran") {
+        //console.log("Valid user");
+        window.location.href ="bank.html";
         
     }
 
-    else{
-        console.log("Invalid user");
+    else {
+        alert("Invalid user");
     }
 })
